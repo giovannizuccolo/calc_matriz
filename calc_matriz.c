656023eaf69932e0f4511ef335d4edd1f6321207
giovannizuccolo <giovannizuccolo.on@gmail.com>
2024-01-01 23:29:34 -0300

    5º commit - upload calc_matriz code & read and write the matriz

---
+++ calc_matriz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+
+    setlocale(LC_ALL, "portuguese");      // setlocale for accents
+
+    int matriz_um[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    int matriz_dois[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    int i, j;
+
+    for (i=0; i < 3; i++) {
+        for (j=0; j < 3; j++) {
+            printf("Digite um valor para a matriz_um na posição a%d, %d", i+1, j+1);
+            scanf("%d" , &matriz_um[i][j]);    
+        }
+    }
+
+    for (i=0; i < 3; i++) {
+        for (j=0; j < 3; j++) {
+            printf("Digite um valor para a matriz_dois na posição a%d, %d", i+1, j+1);
+            scanf("%d" , &matriz_dois[i][j]);    
+        }
+    }
+
+    // exibindo as matrizes
+    for (i=0; i < 3; i++) {
+        for (j=0; j < 3; j++) {
+            printf("%d " , matriz_um[i][j]);    
+        }
+     printf("\n");
+    }
+    
+    printf("--------\n");
+    
+    for (i=0; i < 3; i++) {
+        for (j=0; j < 3; j++) {
+            printf("%d ", matriz_dois[i][j]);    
+        }
+     printf("\n");
+    }
+
+
+
+}
